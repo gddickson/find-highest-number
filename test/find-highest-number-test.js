3bdeb61b03e88eef0dac0a4e01930aa7289395d7
Selvyn <swright@celestial.co.uk>
2021-01-29 15:19:11 +0000

    initial push

---
+++ test/find-highest-number-test.js
@@ -0,0 +1,15 @@
+var assert = require("chai").assert;
+var expect = require("chai").expect;
+
+// Put your CUT here
+
+describe('find the highest number', function() {
+    it('single item in array, return that item', function() {
+        // arrange...
+
+
+        // act...
+
+        // assert...
+    });
+});
